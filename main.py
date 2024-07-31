f991c97966db775a609c1b96929f1b74e1404738
SANEKSANYA <132199360+SANEKSANYA@users.noreply.github.com>
2024-07-31 22:41:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
 from random import choice
+from random import randint
 import os
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -26,6 +28,32 @@ async def mem(ctx):
     await ctx.send(file = picture)
 
 
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+
+
+@bot.command('duck')
+async def duck(ctx):
+    number_random = randint(0, 100)
+
+    if number_random > 30:
+        image_url = get_duck_image_url()
+        await ctx.send(image_url)
+    else:
+        random_image2 = choice(os.listdir('images'))
+
+        with open(f"images/{random_image2}", "rb") as image2:
+            picture2 =  discord.File(image2)
+
+        await ctx.send(file = picture2)  
+        await ctx.send("Ты нашёл секретикккк :)))))")
+
+        
 bot.run('')
 
 
